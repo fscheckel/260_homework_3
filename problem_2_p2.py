903baa4acc255b74f6f298fc750b31831597a0b3
fscheckel <77424041+fscheckel@users.noreply.github.com>
2021-02-10 15:19:01 -1000

    Add files via upload

---
+++ problem_2_p2.py
@@ -0,0 +1,41 @@
+Python 2.7.16 (default, Jan 26 2020, 23:50:38) 
+[GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+"""Problem 2 from Hwk 3 of ASTR260"""
+
+import numpy as np
+
+if __name__ == "__main__":
+    path_to_data = "/Users/mbottom/Desktop/sensor_position_data.txt" #obvsiouly change this to your directory
+    oscillator_data = np.loadtxt(path_to_data, skiprows=1, delimiter=',')
+    #pulls out the first column (remember things start at 0 in python)
+    oscillator_time = oscillator_data[:,0]
+    oscillator_pos  = p(t)=(1)*(sin*(2*pi*(0.02)*t))
+    
+'Problem 2 from Hwk 3 of ASTR260'
+>>> anl_drev(100/100):
+	
+SyntaxError: invalid syntax
+>>> def anl_derv(100/100):
+	
+SyntaxError: invalid syntax
+>>> def anl_derv(100/ 100):
+	
+SyntaxError: invalid syntax
+>>> def anl_derv():
+	return cos*(1)
+f = open("sensor_position_data.txt")
+SyntaxError: invalid syntax
+>>> def anl_derv():
+	return cos*(1)
+f = open("sensor_position_data.txt")def anl_derv():
+	return cos*(1)
+f = open("sensor_position_data.txt", "r")
+SyntaxError: invalid syntax
+>>> def f = open("sensor_position_data.txt")
+SyntaxError: invalid syntax
+>>> def = f = open("sensor_position_data.txt")
+SyntaxError: invalid syntax
+>>> 
